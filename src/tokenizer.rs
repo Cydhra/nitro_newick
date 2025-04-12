8ff3cbee905662b00be3e2b31e203d124b0ac8da
Johannes Hengstler <ubezl@student.kit.edu>
2025-04-12 12:28:48 +0200

    implemented tokenizing quoted strings

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -162,8 +162,31 @@ impl<R: Read> Tokenizer<R> {
     }
 
     fn read_quoted_string(&mut self) -> Result<Token, TokenizerError> {
-        // Implementation of quoted name reading logic goes here
-        Ok(Token::String(String::from("")))
+        self.position += 1;
+        let token = self.read_token(|&b| b == b'\'' || b == b'\n')?;
+        let mut token_string = String::from_utf8_lossy(&token).into_owned();
+        self.position += 1;
+
+        loop {
+            if token_string.ends_with('\n') {
+                return Err(TokenizerError::ParseError {
+                    reason: "Unterminated quoted string".to_string(),
+                });
+            }
+
+            if self.position < self.length && self.buffer[self.position] == b'\'' {
+                self.position += 1; // consume the escaped character, so the predicate works
+                token_string.push('\'');
+                
+                // read until we find a newline or another quote
+                let continued = self.read_token(|&b| b == b'\'' || b == b'\n')?;
+                token_string.push_str(&String::from_utf8_lossy(&continued));
+                self.position += 1;
+            } else {
+                break;
+            }
+        }
+        Ok(Token::String(token_string))
     }
 
     /// Reads from the input stream into the buffer.
