9c72f3e8e2466684e6babcdb17ba2a5b9a2f6e57
Johannes Hengstler <mail.jhengstler@gmail.com>
2025-04-23 16:24:44 +0200

    factor out consuming node infos into functions so we can call it on leaf nodes without closing parentheses as well; give reason for unexpected tokens

--- src/parser.rs
+++ src/parser.rs
@@ -14,7 +14,8 @@ pub enum ParseError {
     /// Unexpected token in the input stream
      UnexpectedToken {
         expected: Vec<Token>,
-        found: Token
+        found: Token,
+        reason: String,
     },
 }
 
@@ -45,6 +46,7 @@ impl<R: Read, B: TreeBuilder> Parser<R, B> {
                         return Err(ParseError::UnexpectedToken {
                             expected: vec![OpenParen, Semicolon],
                             found: token,
+                            reason: "No opening parenthesis found prior".to_string(),
                         });
                     }
 
@@ -56,28 +58,10 @@ impl<R: Read, B: TreeBuilder> Parser<R, B> {
                     // check if the next token is a colon, name, or float, and if so, parse the node
                     // label, support, and branch length
                     if has_info {
-                        // parse node label, support, and branch length
-                        let mut token = self.tokenizer.next_token().context(InputSnafu {})?;
-                        if let Name(label) = token {
-                            node_label = Some(label);
-                            token = self.tokenizer.next_token().context(InputSnafu {})?;
-                        } else if let Float(support) = token {
-                            node_support = Some(support);
-                            token = self.tokenizer.next_token().context(InputSnafu {})?;
-                        }
-
-                        if let Colon = token {
-                            // Ignore colon
-                            let branch_length_token = self.tokenizer.next_token().context(InputSnafu {})?;
-                            if let Float(branch_length) = branch_length_token {
-                                node_branch_length = Some(branch_length);
-                            } else {
-                                return Err(ParseError::UnexpectedToken {
-                                    expected: vec![Float(0.0)],
-                                    found: branch_length_token,
-                                });
-                            }
-                        }
+                        let (label, support, branch_length) = self.consume_named_node_info()?;
+                        node_label = label;
+                        node_support = support;
+                        node_branch_length = branch_length;
                     }
 
                     // pop children from the stack and append to the current node
@@ -91,15 +75,45 @@ impl<R: Read, B: TreeBuilder> Parser<R, B> {
                     if let Some(children) = stack.last_mut() {
                         children.push((node_id, node_support, node_branch_length));
                     }
+                }
+                Name(name) => {
+                    // if we encounter a name, it means there is a named leaf node, because otherwise
+                    // we would have encountered a close parenthesis first, and consumed the
+                    // name
+                    let branch_length = self.consume_branch_length()?;
 
-                    // read away a trailing comma if present
-                    let token = self.tokenizer.peek().context(InputSnafu {})?;
-                    if matches!(token, Comma) {
-                        self.tokenizer.next_token().context(InputSnafu {})?;
+                    // push leaf node to the parent children
+                    if let Some(children) = stack.last_mut() {
+                        let node_id = self.builder.add_node(Some(name));
+                        children.push((node_id, None, branch_length));
+                    } else {
+                        return Err(ParseError::UnexpectedToken {
+                            expected: vec![OpenParen, Semicolon],
+                            found: Name(name),
+                            reason: "No opening parenthesis found prior".to_string(),
+                        });
+                    }
+                },
+                Float(support) => {
+                    // if we encounter a float, it means there is a leaf node with support, because otherwise
+                    // we would have encountered a close parenthesis first, and consumed the
+                    // support
+                    let branch_length = self.consume_branch_length()?;
+
+                    // push leaf node to the parent children
+                    if let Some(children) = stack.last_mut() {
+                        let node_id = self.builder.add_node(None);
+                        children.push((node_id, Some(support), branch_length));
+                    } else {
+                        return Err(ParseError::UnexpectedToken {
+                            expected: vec![OpenParen, Semicolon],
+                            found: Float(support),
+                            reason: "No opening parenthesis found prior".to_string(),
+                        });
                     }
                 }
                 Comma => {
-                    // if we encounter a comma, it means there is a leaf node, because otherwise
+                    // if we encounter a comma, it means there is an unnamed leaf node, because otherwise
                     // we would have encountered a close parenthesis first, and consumed the
                     // comma
 
@@ -113,6 +127,7 @@ impl<R: Read, B: TreeBuilder> Parser<R, B> {
                         return Err(ParseError::UnexpectedToken {
                             expected: vec![OpenParen, Semicolon],
                             found: token,
+                            reason: "No opening parenthesis found prior".to_string(),
                         });
                     }
                 }
@@ -122,9 +137,72 @@ impl<R: Read, B: TreeBuilder> Parser<R, B> {
                     return Err(ParseError::UnexpectedToken {
                         expected: vec![OpenParen, CloseParen, Semicolon, Comma],
                         found: token,
+                        reason: "Unexpected token".to_string(),
                     });
                 }
             }
+        }
+    }
+
+    #[inline]
+    fn consume_named_node_info(&mut self) -> Result<(Option<String>, Option<f64>, Option<f64>), ParseError> {
+        let mut node_label = None;
+        let mut node_support = None;
+        let mut node_branch_length = None;
+
+        // parse node label or support
+        let mut token = self.tokenizer.next_token().context(InputSnafu {})?;
+        if let Name(label) = token {
+            node_label = Some(label);
+        } else if let Float(support) = token {
+            node_support = Some(support);
+        }
+
+        node_branch_length = self.consume_branch_length()?;
+
+        Ok((node_label, node_support, node_branch_length))
+    }
+
+    #[inline]
+    fn consume_branch_length(&mut self) -> Result<Option<f64>, ParseError> {
+        let mut node_branch_length = None;
+
+        if matches!(self.tokenizer.peek(), Ok(Colon)) {
+            // Ignore colon
+            self.tokenizer.next_token().context(InputSnafu {})?;
+
+            let branch_length_token = self.tokenizer.next_token().context(InputSnafu {})?;
+            if let Float(branch_length) = branch_length_token {
+                node_branch_length = Some(branch_length);
+            } else {
+                return Err(ParseError::UnexpectedToken {
+                    expected: vec![Float(0.0)],
+                    found: branch_length_token,
+                    reason: "Expected a branch length after colon".to_string(),
+                });
+            }
+        }
+
+        // read away a trailing comma if present
+        self.consume_trailing_comma()?;
+
+        Ok(node_branch_length)
+    }
+
+    #[inline]
+    fn consume_trailing_comma(&mut self) -> Result<bool, ParseError> {
+        let token = self.tokenizer.peek().context(InputSnafu {})?;
+        if matches!(token, Comma) {
+            self.tokenizer.next_token().context(InputSnafu {})?;
+            Ok(true)
+        } else if matches!(token, CloseParen | Semicolon) {
+            Ok(false)
+        } else {
+            Err(ParseError::UnexpectedToken {
+                expected: vec![Comma, CloseParen, Semicolon],
+                found: token,
+                reason: "Expected a comma, closing parenthesis, or semicolon after a node definition".to_string(),
+            })
         }
     }
 }
